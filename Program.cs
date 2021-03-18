2b582c4a56d72b0903374b435d6ae779e67da62f
Isaac <isaacgeazy@gmail.com>
2021-03-17 21:26:37 -0300

    Atividade Juros Simples

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ent1, ent2, ent3;
+            int t;
+            decimal c, m, j, i;
+
+            Console.Write("Capital: ");
+            ent1 = Console.ReadLine();
+            Console.Write("Taxa de juros: ");
+            ent2 = Console.ReadLine();
+            Console.Write("Tempo (meses): ");
+            ent3 = Console.ReadLine();
+
+            c = Convert.ToDecimal(ent1);
+            i = Convert.ToDecimal(ent2);
+            t = Convert.ToInt16(ent3);
+
+            j = c * i * t;
+            m = (c + j);
+            
+            Console.WriteLine();
+            Console.WriteLine($"Juros simples.: {j}");
+            Console.WriteLine($"Montante.: {m}");
+
+
+        }
+    }
+}
